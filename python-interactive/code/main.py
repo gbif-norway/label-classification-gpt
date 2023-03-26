d56a68d7fabea79bb80f92e5a3c9f27ca0986a4d
rukayaj@gmail.com <rukayaj@gmail.com>
2023-03-26 20:50:06 +0200

    Loop through multiple ocr texts

--- python-interactive/code/main.py
+++ python-interactive/code/main.py
@@ -5,17 +5,19 @@ import requests
 
 # Get some ocr text from somewhere... in this case, the annotater api
 url = os.environ['ANNOTATER_URI']
-query_string = f'{url}?source=gcv_ocr_text&limit=1'
+query_string = f'{url}?source=gcv_ocr_text&notes=ITALY:Test OCR for Padua&limit=1&offset=1'
 response = requests.get(query_string)
-text = response.json()['results'][0]['annotation']
+import pdb; pdb.set_trace()
 
-# ann = "s, is\n1358\n1.49\n3.\nad\n*****\nHERB. PATAV. (PAD)\nMicromeria greu\nprope Lesine\nHD04822\ngręce\n8. pampon fldh\ nMh. pauciflon Us. bub.\nStalio\nSofie & andren\npy Lined\nVISIANI"
-message = { 'ID': 'test-rukaya2', 'Text': text }
+for result in response.json()['results']:
+    # text = "s, is\n1358\n1.49\n3.\nad\n*****\nHERB. PATAV. (PAD)\nMicromeria greu\nprope Lesine\nHD04822\ngręce\n8. pampon fldh\ nMh. pauciflon Us. bub.\nStalio\nSofie & andren\npy Lined\nVISIANI"
+    message = { 'ID': result['resolvable_object_id'], 'Text': result['annotation'] }
 
-# Publish it to the GPT input queue channel (another service which writes to the annotater API is listening for output from this)
-connection_params = pika.ConnectionParameters(host='rabbitmq')
-connection = pika.BlockingConnection(connection_params)
-channel = connection.channel()
-channel.queue_declare(queue=os.environ['INPUT_QUEUE_GPT'])
-channel.basic_publish(exchange='', routing_key=os.environ['INPUT_QUEUE_GPT'], body=json.dumps(message))
+    # Publish it to the GPT input queue channel (another service which writes to the annotater API is listening for output from this)
+    connection_params = pika.ConnectionParameters(host='rabbitmq')
+    connection = pika.BlockingConnection(connection_params)
+    channel = connection.channel()
+    channel.queue_declare(queue=os.environ['INPUT_QUEUE_GPT'])
+    channel.basic_publish(exchange='', routing_key=os.environ['INPUT_QUEUE_GPT'], body=json.dumps(message))
+    
 import pdb; pdb.set_trace()
