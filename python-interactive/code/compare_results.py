4fd71651f6970de2c2affcecee1e84c25dbb13d0
rukayaj@gmail.com <rukayaj@gmail.com>
2023-04-09 12:50:41 +0200

    Improve html comparison table

--- python-interactive/code/compare_results.py
+++ python-interactive/code/compare_results.py
@@ -2,7 +2,10 @@ import os
 import json
 import requests
 import pandas as pd
-import collections
+import re
+import time
+
+FIELDS = ['scientificName', 'catalogNumber', 'recordNumber', 'recordedBy', 'year', 'month', 'day', 'dateIdentified', 'identifiedBy', 'verbatimIdentification', 'country', 'decimalLatitude', 'decimalLongitude', 'location', 'minimumElevationInMeters', 'maximumElevationInMeters', 'verbatimElevation', 'elevation', 'locality']
 
 def generate_html(dataframe: pd.DataFrame):
     table_html = dataframe.to_html(index=False, border=0, table_id="table", classes='table table-striped', justify='left', escape=False, render_links=True).replace('\\n', '<br>')
@@ -22,7 +25,14 @@ def generate_html(dataframe: pd.DataFrame):
             new ImageZoom(container, options1);
         });
         $(document).ready(function () {
-            $('#table').DataTable();
+            $('#table').DataTable( {
+            "columns": [
+                null,
+                null,
+                { "width": "390px" },
+                { "width": "390px" }
+            ]
+            } );
         });
     </script>
     """
@@ -36,12 +46,15 @@ def generate_html(dataframe: pd.DataFrame):
         <style>
         .img-container {{
             max-width: 250px;
-            height: 200px;
+            max-height: 200px;
             display: block;
         }}
         .img-container-wrapper {{ width: 550px; display: block;}}
         tbody tr {{ height: 400px; }}
         body {{ padding: 2em; }}
+        .common {{ background-color: #ddffd6; padding: 2px; }}
+        h4 {{ font-size: 0.8em; font-weight: bold;  }}
+        .different {{ background-color: #ffd6d6; padding: 2px; }}
         </style>
     </header>
     <body>
@@ -51,24 +64,70 @@ def generate_html(dataframe: pd.DataFrame):
     </html>
     """
 
+def get_dwc_fields(record):
+    dwcs = { key:value for (key,value) in record.items() if key in FIELDS and value is not None }
+    if 'elevation' in dwcs:
+        dwcs['verbatimElevation'] = dwcs['elevation']
+        del dwcs['elevation']
+    if 'location' in dwcs:
+        dwcs['locality'] = dwcs['location']
+        del dwcs['location']
+    return dwcs
+
+def image_html(img_url):
+    return f'<div class="img-container-wrapper"><div class="img-container"><img src="{img_url}" class="zoom-image"></div></div>'
+
+def get_dwc_and_img_from_gbif(occurrenceID):
+    query_string = f'https://api.gbif.org/v1/occurrence/search?datasetKey=68a0650f-96ae-499c-8b2a-a4f92c01e4b3&occurrenceID={occurrenceID}&mediaType=StillImage&multimedia=true'
+    response = requests.get(query_string)
+    if response.status_code == requests.codes.ok:
+        results = response.json().get('results')
+        img = results[0]['media'][0]['identifier']
+        return img, get_dwc_fields(results[0])
+    return None, None
+
+def query_url(url):
+    max_retries = 20
+    retry_count = 0
+
+    while retry_count < max_retries:
+        response = requests.get(url)
+        if response.ok:
+            return response.json()['results']
+        else:
+            print(f'retry no {retry_count} failed')
+            retry_count += 1
+            time.sleep(5)
+    
+    raise Exception(f'URL {url} is still inaccessible')
+
 url = os.environ['ANNOTATER_URI']
-response = requests.get(f'{url}?source=gcv_ocr_text&notes=ITALY:Test OCR for Padua&limit=200&offset=0')
-results = response.json()['results']
+# filter = 'source=gcv_ocr_text&notes=ITALY:Test OCR for Padua&limit=200&offset=0' # for Italy specimens
+filter = 'source=gcv_merged_close_blocks&search=urn:catalog:O&limit=200&offset=0' # for GBIF specimens
+results = query_url(f'{url}?{filter}')
 df_dict = {}
 for result in results:
     id = result['resolvable_object_id']
-    cols = {'ocr': result['annotation']}
-    annotation_response = requests.get(f"{url}?resolvable_object_id={id}")
-    annotations = annotation_response.json()['results']
+    print(id)
+    #if not result['annotation'].startswith('UiO : Natural History Museum University of Oslo inches'):
+    ocr = re.sub('UiO : Natural History Museum University of Oslo inches.+\n', '', result['annotation'])
+    # img, gbif_dwc = id, None # for Italy specimens # .str.replace('https://storage.gbif-no.sigma2.no/italy/padua-2023-03-24/', '')
+    img, gbif_dwc = get_dwc_and_img_from_gbif(id) # for GBIF specimens
+    cols = { 'image': image_html(img), 'ocr': ocr }
+    annotations = query_url(f"{url}?resolvable_object_id={id}&source=gpt4")
+    # Should make it so we get the most recent gpt4 annotation maybe?
+    if not annotations:
+        import pdb; pdb.set_trace()
     for annotation in annotations:
-        if annotation['source'] == 'gpt4':
+        if annotation['source'] == 'gpt4': 
             try:
                 gpt4 = annotation['annotation']['choices'][0]['message']['content']
             except KeyError:
                 gpt4 = annotation['annotation']
             gpt4 = {k: v for k, v in gpt4.items() if v}
+            gpt4 = get_dwc_fields(gpt4)
             gpt4 = dict(sorted(gpt4.items()))
-            cols['gpt4'] = '\n'.join([f'{k}: {v}' for k, v in gpt4.items()])
+            break
             # for i in range(3):
             #     if f'gpt4_{i}' not in cols:
             #         cols[f'gpt4_{i}'] = gpt4
@@ -80,11 +139,31 @@ for result in results:
                 del pythondwc['agents']
             pythondwc = dict(sorted(pythondwc.items()))
             #cols['pythondwc_v1'] = '\n'.join([f'{k}: {v}' for k, v in pythondwc.items()])
+    if gbif_dwc:
+        all_keys = set(gbif_dwc.keys()) | set(gpt4.keys())
+        gbif_dwc = {key: gbif_dwc.get(key, None) for key in all_keys}
+        gpt4 = {key: gpt4.get(key, None) for key in all_keys}
+        #cols['gbif_dwc'] = '\n'.join([f'{k}: {v}' for k, v in dict(sorted(gbif_dwc.items())).items()])
+        #cols['gpt4'] = '\n'.join([f'{k}: {v}' for k, v in dict(sorted(gpt4.items())).items()])
+        gbif_items = dict(sorted(gbif_dwc.items()))
+        common = [f'{k}: {v}' for k, v in gbif_items.items() if str(gpt4[k]).lower() == str(gbif_dwc[k]).lower()]
+        if 'scientificName' not in common and gbif_items['scientificName'] is not None and gpt4['scientificName'] is not None: 
+            if ' '.join(gbif_items['scientificName'].split(' ')[0:2]).lower() == ' '.join(gpt4['scientificName'].split(' ')[0:2]).lower():
+                common.append(f"scientificName: {gbif_items['scientificName'].split(' ')[0:2]}")
+                del gbif_items['scientificName']
+                del gpt4['scientificName']
+        common_dwcs = '\n'.join(common)
+        gbif_items_text = '\n'.join([f'{k}: {v}' for k, v in gbif_items.items() if str(gpt4[k]).lower() != str(gbif_dwc[k]).lower()])
+        cols['gbif_dwc'] = f'<div class="common"><h4>Common</h4>{common_dwcs}</div><div class="different"><h4>Differing</h4>{gbif_items_text}</div>'
+        gpt4_items = dict(sorted(gpt4.items()))
+        gpt4_items_text = '\n'.join([f'{k}: {v}' for k, v in gpt4_items.items() if str(gpt4[k]).lower() != str(gbif_dwc[k]).lower()])
+        cols['gpt4'] = f'<div class="common"><h4>Common</h4>{common_dwcs}</div><div class="different"><h4>Differing</h4>{gpt4_items_text}</div>'
+    else:
+        cols['gpt4'] = '\n'.join([f'{k}: {v}' for k, v in dict(sorted(gpt4.items())).items()])
     df_dict[id] = cols
 
 table = pd.DataFrame.from_dict(df_dict, orient='index')
-table.insert(0, 'image', '<div class="img-container-wrapper"><div class="img-container"><img src="' + table.index + '" class="zoom-image"></div></div>')    # .str.replace('https://storage.gbif-no.sigma2.no/italy/padua-2023-03-24/', '')
-with open("index.html", "w") as writer:
+with open("index-uio.html", "w") as writer:
     writer.write(generate_html(table))
 import pdb; pdb.set_trace()
 table.to_csv('test.csv', index=False)
