c5c51f86331af058d4235fd51e884b3e947a113f
rukayaj@gmail.com <rukayaj@gmail.com>
2023-04-13 20:03:57 +0200

    Copy gapi secret file and print out some debugging information

--- gcv-ocr/code/api/gcv.go
+++ gcv-ocr/code/api/gcv.go
@@ -9,6 +9,8 @@ import (
 )
 
 func SendToOCR(imageURL string) (*pb.AnnotateImageResponse, error) {
+	fmt.Printf("Sending image URL to OCR: %s\n", imageURL)
+
 	ctx := context.Background()
 	client, err := vision.NewImageAnnotatorClient(ctx)
 	if err != nil {
@@ -36,6 +38,11 @@ func SendToOCR(imageURL string) (*pb.AnnotateImageResponse, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to send image to OCR: %v", err)
 	}
+	if response.Error != nil {
+		fmt.Printf("API response error: %v\n", response.Error)
+	}
+
+	fmt.Printf("API response: %+v\n", response)
 
 	return response, nil
 }
