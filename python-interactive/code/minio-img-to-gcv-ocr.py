8eaef9063a308fd3fb13545fbe5cc859ead755a8
rukayaj@gmail.com <rukayaj@gmail.com>
2023-07-24 07:54:12 +0200

    Add download image option for when gcv complains it cannot access the image

--- python-interactive/code/minio-img-to-gcv-ocr.py
+++ python-interactive/code/minio-img-to-gcv-ocr.py
@@ -8,8 +8,8 @@ def get_images(bucket_name, prefix):
     objects = minio_client.list_objects(bucket_name, prefix=prefix, recursive=True)
     return [obj.object_name for obj in objects if obj.object_name.endswith((".jpg", ".jpeg", ".png"))]
  
-bucket_name = "img"
-prefix = "uio-algae/"
+bucket_name = "test"
+prefix = "test/TNU/"
 image_names = get_images(bucket_name, prefix)
 
 connection_params = pika.ConnectionParameters(host='rabbitmq')
@@ -17,10 +17,11 @@ connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
 channel.queue_declare(queue=os.environ['INPUT_QUEUE_GCV_OCR'])
 
+import pdb; pdb.set_trace()
+
 for image_name in image_names:
     minio_url = f"https://{os.getenv('MINIO_URI')}/{bucket_name}/{image_name}"
     body = json.dumps({ 'ID': minio_url, 'Text': minio_url, 'Source': 'minio'})
-    import pdb; pdb.set_trace()
     channel.basic_publish(exchange='', routing_key=os.environ['INPUT_QUEUE_GCV_OCR'], body=body)
 
 
